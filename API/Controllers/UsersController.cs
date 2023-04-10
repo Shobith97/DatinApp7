853b1878b72ca30338de1b4959df0b8744f3d332
shobith <shobithbandaru@gmail.com>
2023-04-10 19:14:24 -0400

    end of section

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]///  
+    public class UsersController
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+    }
+}
